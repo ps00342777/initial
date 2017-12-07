d1440e88b6b0d768b21d1e633ed9fa3a7ea6afe8
Prasad Chandrakant Surve <PS00342777@techmahindra.com>
2017-12-07 16:47:31 +0530

    initial commit

---
+++ release-test/src/main/java/com/telia/communication/releasetest/ReleaseTestApplication.java
@@ -0,0 +1,12 @@
+package com.telia.communication.releasetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseTestApplication.class, args);
+	}
+}
